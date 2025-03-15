086086320cdbf7308b2f4e9db9c04a44d041a44e
Shriyash Dongarkar <shriyashdongarkar@gmail.com>
2025-03-15 22:15:51 +0530

    Schema Viewer Changes

--- src/components/SchemaViewer.jsx
+++ src/components/SchemaViewer.jsx
@@ -2,7 +2,7 @@ export default function SchemaViewer() {
   return (
     <div className="w-full h-screen p-4">
       <iframe
-        src="https://dbdocs.io/shriyashdongarkar1104/Inspiron-Frontend"
+        src="https://dbdocs.io/shriyashdongarkar1104/Inspiron-Frontend?view=relationships"
         width="100%"
         height="100%"
         className="border rounded-lg shadow-md min-h-[80vh]"
